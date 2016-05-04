9c3c139c13d5ebdc9ae5531d487c7728a1ca9a22
Julien Maulny <doubjulien@hotmail.fr>
2016-05-04 11:35:28 +0200

    install fosrest with jms serializer

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -13,9 +13,11 @@ class AppKernel extends Kernel
             new Symfony\Bundle\TwigBundle\TwigBundle(),
             new Symfony\Bundle\MonologBundle\MonologBundle(),
             new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
+            new JMS\SerializerBundle\JMSSerializerBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new FOS\UserBundle\FOSUserBundle(),
+            new FOS\RestBundle\FOSRestBundle(),
             new CoreBundle\CoreBundle(),
         ];
 
