6cc8b3be916965b85f61660871c720ec28f36de7
Julien Maulny <doubjulien@hotmail.fr>
2016-05-04 15:29:49 +0200

    add post origin action

--- src/CoreBundle/Controller/OriginController.php
+++ src/CoreBundle/Controller/OriginController.php
@@ -2,10 +2,22 @@
 
 namespace CoreBundle\Controller;
 
+use Doctrine\DBAL\Exception\UniqueConstraintViolationException;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\ConflictHttpException;
+use FOS\RestBundle\View\View;
 use FOS\RestBundle\Controller\FOSRestController;
+use CoreBundle\Entity\Origin;
+use CoreBundle\Form\OriginType;
 
 class OriginController extends FOSRestController
 {
+    /**
+     * Retrieve all origins.
+     *
+     * @return View
+     */
     public function getOriginsAction()
     {
         $origins = $this->getDoctrine()
@@ -13,6 +25,36 @@ class OriginController extends FOSRestController
             ->findAll()
         ;
 
-        return $origins;
+        return View::create($origins);
+    }
+
+    /**
+     * @param Request $request
+     *
+     * @return View id of the created origin.
+     *
+     * @throws ConflictHttpException If name (unique) already exists.
+     */
+    public function postOriginsAction(Request $request)
+    {
+        $entity = new Origin();
+        $form = $this->createForm(OriginType::class, $entity);
+
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+
+            try {
+                $em->persist($entity);
+                $em->flush();
+            } catch (UniqueConstraintViolationException $e) {
+                throw new ConflictHttpException($e->getMessage(), $e);
+            }
+
+            return View::create($entity->getId(), Response::HTTP_CREATED);
+        }
+
+        return View::create($form, Response::HTTP_UNPROCESSABLE_ENTITY);
     }
 }
