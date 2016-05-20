4d59b2d9cbdee95e4fd8ce2de3fa080a3f91c778
Rémy Abi-Khalil <remyabi@gmail.com>
2016-05-20 10:04:12 +0200

    Refs #NAVIO-624 ~ findOrCreateByName function added in OriginManager.

--- src/ApiBundle/Service/OriginManager.php
+++ src/ApiBundle/Service/OriginManager.php
@@ -31,6 +31,20 @@ class OriginManager
         return $this->repository->find($id);
     }
 
+    public function findOrCreateByName($name)
+    {
+        $origin = $this->repository->findOneByName($name);
+
+        if (is_null($origin)) {
+            $origin = new Origin();
+
+            $origin->setName($name);
+            $this->em->persist($origin);
+            $this->em->flush();
+        }
+        return $origin;
+    }
+
     public function findAll()
     {
         return $this->repository->findAll();
